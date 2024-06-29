6704c191cb71baf0e1066adb10c5691b328fe07d
iida <iida.lintuaho@gmail.com>
2024-06-29 18:58:34 +0300

    base for board interaction. tile hover and clicking

--- Assets/Scripts/GameScripts/BoardCreator.cs
+++ Assets/Scripts/GameScripts/BoardCreator.cs
@@ -1,28 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
 public class BoardCreator : MonoBehaviour
 {
+    //TODO rename and/or restructure this/other boardmanaging logic scripts
+    private TileControl[,] m_tiles;
     [SerializeField] private int m_xWidth, m_zWidth;
-    private float m_tileOffset = 0.15f;
+    private readonly float m_tileOffset = 0.15f;
     [SerializeField] private GameObject m_tilePrefab;
+    public TileControl m_selectedTile;
+
 
     void Start()
     {
+        m_tiles = new TileControl[m_xWidth, m_zWidth];
         for (int i = 0; i < m_xWidth; i++)
         {
             for (int j = 0; j < m_zWidth; j++)
             {
                 GameObject tile = Instantiate(m_tilePrefab, new Vector3(i+(m_tileOffset * i), 0, j + (m_tileOffset* j)), m_tilePrefab.transform.rotation, this.transform);
                 tile.name = $"tile{i}{j}";
+                TileControl controller = tile.GetComponent<TileControl>();
+                controller.InitTile(new Vector2(j,i), this);
+                controller.m_onTileSelect += TileClicked;
+                controller.m_onTileDeselect += TileUnclicked;
+                m_tiles[j, i] = tile.GetComponent<TileControl>();
             }
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    void TileClicked(TileControl tile)
     {
+        if (m_selectedTile != null) 
+        {
+            //other tile has been selected, do nothing
+            return;            
+        }
+        tile.TileSelected();
+        m_selectedTile = tile;
+    }
 
+    void TileUnclicked(TileControl tile)
+    {
+        if (m_selectedTile != tile)
+        {
+            return;            
+        }
+        tile.TileDeselected();
+        m_selectedTile = null;
+    }
+
+    public bool IsBoardIdling()
+    {
+        //TODO probs rename, but when other logic is added to here
+        if(m_selectedTile == null){
+            return true;
+        }
+        return false;
     }
 }
---
+++ Assets/Scripts/GameScripts/TileControl.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class TileControl : MonoBehaviour
+{
+    public UnityAction<TileControl> m_onTileSelect;
+    public UnityAction<TileControl> m_onTileDeselect;
+    //coordinates
+    [SerializeField] GameObject m_highlightHover;
+    [SerializeField] GameObject m_highlightClicked;
+
+    Vector2 m_coordinates;
+    BoardCreator m_boardManager;
+    bool m_mouseHover = false;
+    public bool m_tileSelected = false;
+
+    void Start()
+    {
+        m_highlightHover.SetActive(false);
+        m_highlightClicked.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        if (m_boardManager.IsBoardIdling())
+        {
+            // no tile has been selected, do hover highlighting
+            TileHovering();
+        }
+    }
+    void TileHovering()
+    {
+        if (m_mouseHover)
+        {
+            m_highlightHover.SetActive(true);
+        }
+        else
+        {
+            m_highlightHover.SetActive(false);
+        }
+    }
+
+    public void InitTile(Vector2 coordinates, BoardCreator manager)
+    {
+        m_coordinates = coordinates;
+        m_boardManager = manager;
+    }
+
+    public void TileSelected()
+    {
+        m_tileSelected = true;
+        m_highlightClicked.SetActive(true);
+        m_highlightHover.SetActive(false);
+    }
+    public void TileDeselected()
+    {
+        m_tileSelected = false;
+        m_highlightClicked.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        if (!m_tileSelected)
+        {
+            m_onTileSelect.Invoke(this);
+        }
+        else
+        {
+            m_onTileDeselect.Invoke(this);
+        }
+    }
+    void OnMouseEnter()
+    {
+        m_mouseHover = true;
+    }
+
+    void OnMouseExit()
+    {
+        m_mouseHover = false;
+    }
+}
